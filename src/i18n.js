95f787ba67a6cfdb9acd9be3c0b399ae1837e907
Adam Miller <adam@milrr.com>
2019-07-23 13:45:49 -0500

    add cache busting at build

--- src/i18n.js
+++ src/i18n.js
@@ -1,19 +1,27 @@
 import i18n from 'i18next';
 import XHR from 'i18next-xhr-backend';
 import { initReactI18next } from 'react-i18next';
+import preval from 'preval.macro';
 
 i18n
   .use(XHR)
   .use(initReactI18next)
   .init({
     lng: 'en',
+    fallbackLng: 'en',
     load: 'currentOnly',
     //debug: true,
     interpolation: {
       escapeValue: false
     },
+    react: {
+      wait: true
+    },
     backend: {
-      loadPath: `${process.env.PUBLIC_URL}/locale/{{lng}}.json` //TODO: cache busting
+      loadPath: `${process.env.PUBLIC_URL}/locale/{{lng}}.json`,
+      queryStringParams: {
+        v: preval`module.exports = Date.now();` // add a build time querystring for cache busting
+      }
     }
   });
 
